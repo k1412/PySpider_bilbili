85552e191be8a8d4a07eed0aa933e1c190b39951
k1412 <w.y.v@live.com>
2019-07-15 17:45:32 +0800

    linux add

---
+++ functionTest_01.py
@@ -0,0 +1,14 @@
+'''
+链接数据库，联系数据库的使用
+爬取番剧的类别信息
+尝试爬一页个人数据
+'''
+
+#练习数据库的使用
+import pymysql
+
+db = pymysql.connect(host= 'www.k1412.top',user= 'wy-remote',password= 'dovewyjzrn123',port= 3306)
+cursor = db.cursor()
+cursor.execute('SELECT VERSION()')
+data = cursor.fetchone()
+print('Database version:',data)
