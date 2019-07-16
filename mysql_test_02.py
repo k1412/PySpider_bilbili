c1ab6e525b9584e2a09abe302fe4653b6e22d079
k1412 <w.y.v@live.com>
2019-07-16 11:49:30 +0800

    linux add

--- mysql_test_02.py
+++ mysql_test_02.py
@@ -16,8 +16,10 @@ import requests
 
 #读取练习：
 
-connect_info = 'mysql+pymysql://wy-remote:dovewyjzrn123@www.k1412.top:3306/spiders_test?charset=utf8'
-engine = create_engine(connect_info)
-data = pd.read_sql_table(table_name= 'test1',con = engine)
-print data
-data.to_csv('mysql_read_test.csv',encoding='utf-8',index=False)
+# connect_info = 'mysql+pymysql://wy-remote:dovewyjzrn123@www.k1412.top:3306/spiders_test?charset=utf8'
+# engine = create_engine(connect_info)
+# data = pd.read_sql_table(table_name= 'test1',con = engine)
+# print data
+# data.to_csv('mysql_read_test.csv',encoding='utf-8',index=False)
+
+#结合练习： 待做，优先解决，单一用户追番信息的爬取，以及补充番剧信息的爬取
